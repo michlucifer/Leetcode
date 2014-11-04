bebf00728fa8f194bd843360a62b14eaed4df264
michlucifer <juehou4102@gmail.com>
2014-11-04 15:00:17 -0500

    Create PreOrderTraversal.java

---
+++ PreOrderTraversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> returnList = new ArrayList<Integer>();
+        
+        if(root == null)
+        return returnList;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        
+        while(!stack.empty()) {
+            TreeNode temp = stack.pop();
+            returnList.add(temp.val);
+            
+            if(temp.right != null) {
+                stack.push(temp.right);
+            }
+            if(temp.left != null) {
+                stack.push(temp.left);
+            }
+            
+        }
+        
+        return returnList;
+        
+    }
+}
