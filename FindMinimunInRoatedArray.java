c0b20fe3a807dfda79b260055edd045aec393bb0
michlucifer <juehou4102@gmail.com>
2015-06-12 10:12:35 +0800

    Create FindMinimunInRoatedArray.java

---
+++ FindMinimunInRoatedArray.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int findMin(int[] num) {
+       if(num == null || num.length==0)
+        return 0;
+    int l = 0;
+    int r = num.length-1;
+    int min = num[0];
+    while(l<r-1)
+    {
+        int m = (l+r)/2;
+        if(num[l]<num[m])
+        {
+            min = Math.min(num[l],min);
+            l = m+1;
+        }
+        else if(num[l]>num[m])
+        {
+            min = Math.min(num[m],min);
+            r = m-1;
+        }
+        else
+        {
+            l++;
+        }
+    }
+    min = Math.min(num[r],min);
+    min = Math.min(num[l],min);
+    return min;
+    }
+}
